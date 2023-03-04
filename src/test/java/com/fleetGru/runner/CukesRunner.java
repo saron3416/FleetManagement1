47a77514bd44b32b695ba0260e6a777ff796ae8e
bermetk <butterfly280886@gmail.com>
2023-03-03 22:54:24 -0600

    classes added

---
+++ src/test/java/com/fleetGru/runner/CukesRunner.java
@@ -0,0 +1,23 @@
+package com.fleetGru.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "pretty",
+                "json:target/cucumber.json",
+                "html:target/cucumber-reports.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features",
+        glue = "com/cydeo/step_definitions",
+        dryRun = false,
+        tags = "@smoke",
+        publish = true //generating a report with public link
+        // ,monochrome = false
+)
+public class CukesRunner {}
